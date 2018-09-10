a9f8f87deb12aa8c9f1b94af3a071a91900d7e25
brian91292 <8050523+brian91292@users.noreply.github.com>
2018-09-10 16:48:04 -0500

    first commit

---
+++ MapperFeed/MapperFeed.cs
@@ -0,0 +1,181 @@
+﻿using SongLoaderPlugin;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace CustomSongSync {
+    class MapperFeed : MonoBehaviour {
+        private bool _downloaderRunning = false;
+        private Stack<string> _artistDownloadQueue = new Stack<string>();
+
+        void Awake() {
+            UnityEngine.Object.DontDestroyOnLoad(this.gameObject);
+
+            string favoriteMappersPath = $"{Environment.CurrentDirectory}\\UserData\\FavoriteMappers.ini";
+            if (!File.Exists(favoriteMappersPath)) {
+                File.WriteAllLines(favoriteMappersPath, new string[] { "freeek", "purphoros", "bennydabeast" });
+            }
+            
+            foreach (string mapper in File.ReadAllLines(favoriteMappersPath)) {
+                Plugin.Log($"Mapper: {mapper}");
+                _artistDownloadQueue.Push(mapper);
+            }
+        }
+
+        void Update() {
+            if (_artistDownloadQueue.Count > 0 && !_downloaderRunning) {
+                StartCoroutine(DownloadAllSongsByAuthor(_artistDownloadQueue.Pop()));
+            }
+            else if (_artistDownloadQueue.Count == 0 && !_downloaderRunning) {
+                SongLoader.Instance.RefreshSongs(false);
+                Plugin.Log("Finished updating songs from all mappers!");
+                Destroy(this.gameObject);
+            }
+        }
+
+        public void Empty(System.IO.DirectoryInfo directory) {
+            foreach (System.IO.FileInfo file in directory.GetFiles()) file.Delete();
+            foreach (System.IO.DirectoryInfo subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
+        }
+        
+        string GetAuthorID(string author, byte[] rawData) {
+            string searchResult = System.Text.Encoding.Default.GetString(rawData);
+            string search = $">{author.ToLower()}<";
+
+            while (true) {
+                int index = searchResult.IndexOf(search);
+                if (index != -1) {
+                    string tmp = searchResult.Substring(0, index - 1);
+                    return tmp.Substring(tmp.LastIndexOf('/') + 1);
+                }
+                else {
+                    break;
+                }
+            }
+            return String.Empty;
+        }
+
+        IEnumerator DownloadAllSongsByAuthor(string author) {
+            _downloaderRunning = true;
+            using (UnityWebRequest www = UnityWebRequest.Get($"https://beatsaver.com/search/all/{author}")) {
+                yield return www.SendWebRequest();
+                if (www.isNetworkError || www.isHttpError) {
+                    Plugin.Log(www.error);
+                }
+                else {
+                    byte[] data = www.downloadHandler.data;
+                    string authorID = GetAuthorID(author, data);
+                    yield return DownloadSongs(author, authorID);
+                }
+            }
+            _downloaderRunning = false;
+        }
+
+        IEnumerator DownloadFile(string url, string path) {
+            using (UnityWebRequest www = UnityWebRequest.Get(url)) {
+                yield return www.SendWebRequest();
+                if (www.isNetworkError || www.isHttpError) {
+                    Plugin.Log(www.error);
+                }
+                else {
+                    File.WriteAllBytes(path, www.downloadHandler.data);
+                }
+            }
+        }
+
+        IEnumerator DownloadSongs(string author, string authorID) {
+            var startTime = DateTime.Now;
+            int downloadCount = 0;
+            int totalSongs = 0;
+            int currentSongIndex = 0;
+
+            if (!Directory.Exists(".songcache")) {
+                Directory.CreateDirectory(".songcache");
+            }
+            
+            if (!Directory.Exists("CustomSongs")) {
+                Directory.CreateDirectory("CustomSongs");
+            }
+
+            while (true) {
+                bool exitLoop = false;
+                string url = $"https://beatsaver.com/browse/byuser/{authorID}/{currentSongIndex.ToString()}";
+                using (UnityWebRequest www = UnityWebRequest.Get(url)) {
+                    yield return www.SendWebRequest();
+                    if (www.isNetworkError || www.isHttpError) {
+                        Plugin.Log(www.error);
+                    }
+                    else {
+                        string searchResult = www.downloadHandler.text;
+
+                        string search = "/browse/detail/";
+                        List<string> songIndices = new List<string>();
+                        while (true) {
+                            int index = searchResult.IndexOf(search);
+                            if (index != -1) {
+                                searchResult = searchResult.Substring(index + search.Length);
+                                string songIndex = searchResult.Substring(0, searchResult.IndexOf('"'));
+                                if (!songIndices.Contains(songIndex)) {
+                                    songIndices.Add(songIndex);
+                                }
+                            }
+                            else {
+                                break;
+                            }
+                        }
+
+                        if (songIndices.Count == 0) {
+                            exitLoop = true;
+                            break;
+                        }
+                        
+                        foreach (string songIndex in songIndices) {
+                            string songDirectory = $"{Environment.CurrentDirectory}\\CustomSongs\\{songIndex}";
+
+                            if (!Directory.Exists(songDirectory)) {
+                                Empty(new DirectoryInfo(".songcache"));
+
+                                string localPath = $"{Environment.CurrentDirectory}\\.songcache\\{songIndex}.zip";
+                                yield return DownloadFile($"https://beatsaver.com/download/{songIndex}", localPath);
+
+                                ZipFile.ExtractToDirectory(localPath, ".songcache");
+
+                                yield return null;
+
+                                File.Delete(localPath);
+
+                                string[] directories = Directory.GetDirectories($"{Environment.CurrentDirectory}\\.songcache");
+                                foreach (var directory in directories) {
+                                    Directory.Move(directory, songDirectory);
+                                }
+
+                                downloadCount++;
+                            }
+                            totalSongs++;
+                        }
+                        currentSongIndex += 20;
+                    }
+                }
+                
+                if (exitLoop) {
+                    break;
+                }
+            }
+            Empty(new DirectoryInfo(".songcache"));
+            Directory.Delete(".songcache");
+
+            Plugin.Log($"Downloaded {downloadCount.ToString()} songs from {author} in {((DateTime.Now - startTime).Seconds.ToString())} seconds. Skipped {(totalSongs-downloadCount).ToString()} songs.");
+        }
+    }
+}
---
+++ MapperFeed/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using IllusionPlugin;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CustomSongSync {
+    public class Plugin : IPlugin {
+        public string Name => "MapperFeed";
+        public string Version => "0.0.1";
+        public void OnApplicationStart() {
+            new GameObject().AddComponent<MapperFeed>();
+        }
+
+        public void OnApplicationQuit() {
+        }
+
+        public void OnLevelWasLoaded(int level) {
+
+        }
+
+        public void OnLevelWasInitialized(int level) {
+        }
+
+        public void OnUpdate() {
+        }
+
+        public void OnFixedUpdate() {
+        }
+
+        public static void Log(string msg) {
+            msg = $"[MapperFeed] {msg}";
+            Console.WriteLine(msg);
+        }
+    }
+}
