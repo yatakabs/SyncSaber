427a9c1f117d3e1666e9779128e9f73e95a8e368
brian91292 <8050523+brian91292@users.noreply.github.com>
2018-10-12 12:31:17 -0500

    Update version

--- BeatSaberMapperFeed/Plugin.cs
+++ BeatSaberMapperFeed/Plugin.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 namespace BeatSaberMapperFeed {
     public class Plugin : IPlugin {
         public string Name => "MapperFeed";
-        public string Version => "1.0.1";
+        public string Version => "1.0.2";
         public void OnApplicationStart() {
             new GameObject().AddComponent<MapperFeed>();
         }
